cfc3c82ef4a8175478448721f494a886c8902fc6
chaowen guo <chaowen.guo1@gmail.com>
2021-02-08 05:47:15 -0800

    Update job.py

--- job.py
+++ job.py
@@ -1,7 +1,7 @@
 import aiohttp, asyncio
     
 async def f():
-    async with aiohttp.ClientSession() as session: 
-        async with session.get('https://v2ex.com/go/jobs') as response: print(await response.text())
+    async with aiohttp.ClientSession() as session:
+        for _ in range(1, 1000): async with session.get(f'https://v2ex.com/go/jobs?p=') as response: print(await response.text())
 
 asyncio.run(f())
