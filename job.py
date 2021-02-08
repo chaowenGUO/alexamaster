1ae3c507c642c5da6ba066cd693e169ab602e56b
chaowen guo <chaowen.guo1@gmail.com>
2021-02-08 06:41:07 -0800

    Update job.py

--- job.py
+++ job.py
@@ -5,6 +5,6 @@ async def f():
         for _ in range(1, 1000): 
             async with session.get(f'https://v2ex.com/go/jobs?p={_}') as response: 
                 soup = bs4.BeautifulSoup(await response.text())
-                soup.
+                print(soup.select(''))
 
 asyncio.run(f())
