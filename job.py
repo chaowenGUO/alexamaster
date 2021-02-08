9539c7d1ee59a7ed8e8a075ef7acb390fd66f4ef
chaowen guo <chaowen.guo1@gmail.com>
2021-02-08 04:29:22 -0800

    Update job.py

--- job.py
+++ job.py
@@ -1,4 +1,7 @@
 import aiohttp
-
-async def f():
     
+async def f():
+    async with aiohttp.ClientSession() as session: 
+        async with session.post(f'https://api.github.com/repos/chaowenGUOorg/{_}/dispatches') as _: pass
+
+asyncio.run(f())
