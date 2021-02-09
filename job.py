b495e65a3c9e2daceb10181f7e0a2749b78020b1
chaowen guo <chaowen.guo1@gmail.com>
2021-02-08 18:07:18 -0800

    Update job.py

--- job.py
+++ job.py
@@ -2,11 +2,12 @@ import aiohttp, asyncio, bs4
     
 async def f():
     async with aiohttp.ClientSession() as session:
-        for _ in range(1, 1000): 
+        for _ in range(1, 10): 
             async with session.get(f'https://v2ex.com/go/jobs?p={_}') as response: 
-                soup = bs4.BeautifulSoup(await response.text())
-                print(soup.select('.item_title'))
-        async with session.get(f'https://v2ex.com/go/jobs?p=150') as response: 
-            print(await response.text())
+                //soup = bs4.BeautifulSoup(await response.text())
+                //print(soup.select('.item_title'))
+                print(await response.text())
+        //async with session.get(f'https://v2ex.com/go/jobs?p=150') as response: 
+            //print(await response.text())
 
 asyncio.run(f())
