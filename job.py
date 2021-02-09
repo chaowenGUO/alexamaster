802bba000f713643e2397aee0a1944a080d812fc
chaowen guo <chaowen.guo1@gmail.com>
2021-02-08 23:02:50 -0800

    Update job.py

--- job.py
+++ job.py
@@ -1,3 +1,9 @@
+import cfscrape
+
+scraper = cfscrape.create_scraper()  # returns a CloudflareScraper instance
+# Or: scraper = cfscrape.CloudflareScraper()  # CloudflareScraper inherits from requests.Session
+print(scraper.get('https://v2ex.com/go/jobs').content)
+
 import aiohttp, asyncio, bs4
     
 async def f():
