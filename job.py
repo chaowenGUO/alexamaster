a464ab4f9b2c143ee263be8afe3519675b2a4319
chaowen guo <chaowen.guo1@gmail.com>
2021-02-21 04:14:00 -0800

    Update job.py

--- job.py
+++ job.py
@@ -1,7 +1,7 @@
 import selenium.webdriver, pathlib
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
-options.add_arguments('--disable-popup-blocking');
+options.add_arguments('--disable-popup-blocking')
 driver = selenium.webdriver.Chrome(chrome_options=options)
 driver.get('https://www.alexamaster.net/Master/157701')
 pathlib.Path('index.html').write_text(driver.page_source)
