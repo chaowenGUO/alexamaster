c2883b3be0db61a5064b4640ac84f10a6204cb72
chaowen guo <chaowen.guo1@gmail.com>
2021-02-21 03:38:32 -0800

    Update job.py

--- job.py
+++ job.py
@@ -2,7 +2,6 @@ import selenium.webdriver, pathlib
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 driver = selenium.webdriver.Chrome(chrome_options=options)
-driver.implicitly_wait(120)
 driver.get('https://www.alexamaster.net/Master/157701')
 pathlib.Path('index.html').write_text(driver.page_source)
 driver.close()
