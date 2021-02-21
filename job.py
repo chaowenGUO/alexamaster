c6a086ef7e3f233e62c1220ba632a47d77c25ce7
chaowen guo <chaowen.guo1@gmail.com>
2021-02-21 03:11:52 -0800

    Update job.py

--- job.py
+++ job.py
@@ -1,8 +1,8 @@
-import selenium.webdriver
+import selenium.webdriver, pathlib
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 driver = selenium.webdriver.Chrome(chrome_options=options)
 driver.implicitly_wait(120)
 driver.get('https://www.alexamaster.net/Master/157701')
-driver.save_screenshot('screenshot.png')
+pathlib.Path('ha.html').write_file(driver.page_source)
 driver.close()
