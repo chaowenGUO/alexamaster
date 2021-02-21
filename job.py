046ec1c1d6e5767dff962157092979d17ba596a0
chaowen guo <chaowen.guo1@gmail.com>
2021-02-21 03:15:58 -0800

    Update job.py

--- job.py
+++ job.py
@@ -4,5 +4,5 @@ options.headless = True
 driver = selenium.webdriver.Chrome(chrome_options=options)
 driver.implicitly_wait(120)
 driver.get('https://www.alexamaster.net/Master/157701')
-pathlib.Path('ha.html').write_file(driver.page_source)
+pathlib.Path('ha.html').write_text(driver.page_source)
 driver.close()
