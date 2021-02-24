073709b20f663b3ea95fe377dcf94cf7236d4f7f
chaowen guo <chaowen.guo1@gmail.com>
2021-02-24 00:22:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import selenium.webdriver
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--disable-popup-blocking')
+options.add_argument('--incognito')
 driver = selenium.webdriver.Chrome(chrome_options=options)
 driver.get('https://www.alexamaster.net/Master/157701')
 while True: pass
