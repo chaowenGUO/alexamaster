304c4129cc07194a9a49efc2f57af009c5339880
chaowen guo <chaowen.guo1@gmail.com>
2021-02-25 02:08:23 -0800

    Update good.py

--- good.py
+++ good.py
@@ -4,9 +4,8 @@ options.headless = True
 options.add_argument('--disable-popup-blocking')
 options.add_argument('--incognito')
 options.add_argument('--no-first-run')
-options.add_argument('--start-maximized')
 driver = selenium.webdriver.Chrome(options=options)
-driver.maximize_window()
+driver.execute_script('globalThis.scrollTo(0,1000)'）
 driver.get('https://www.alexamaster.net/sec/login.php')
 driver.find_element_by_css_selector('[placeholder="Your Email ..."]').send_keys('chaowen.guo1@gmail.com')
 driver.find_element_by_css_selector('[placeholder="Your Password ..."]').send_keys('HL798820y+')
