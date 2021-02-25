e7704e3b99a48ac975455e2607d53350f0e8d877
chaowen guo <chaowen.guo1@gmail.com>
2021-02-25 04:36:27 -0800

    Update good.py

--- good.py
+++ good.py
@@ -1,9 +1,8 @@
-import selenium.webdriver, time, PIL, io, asyncio, aiohttp
+import selenium.webdriver, time, PIL, io, asyncio, aiohttp, pytesseract
 async def f():
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://www.alexamaster.net/sec/login.php') as response:
-            with io.BytesIO(await response.content.read()) as _:               
-asyncio.run(f())
+        async with session.get('https://www.alexamaster.net/sec/image.php') as response:
+            return pytesseract.image_to_string(PIL.Image.open(io.BytesIO(await response.content.read())))             
 
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
@@ -19,4 +18,5 @@ driver.find_element_by_css_selector('[placeholder="Your Password ..."]').send_ke
 driver.find_element_by_css_selector('[value="Login Now"]').click()
 time.sleep(60)
 driver.save_screenshot('good.png')
+print(asyncio.run(f()))
 driver.close()
