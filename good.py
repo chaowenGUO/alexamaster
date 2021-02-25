d769a40a08c33ea2e500d5be6c869037c31f297f
chaowen guo <chaowen.guo1@gmail.com>
2021-02-25 06:56:51 -0800

    Update good.py

--- good.py
+++ good.py
@@ -1,11 +1,5 @@
 import selenium.webdriver, time, PIL, io, pytesseract
-#async def f():
-#    async with aiohttp.ClientSession() as session:
-#        async with session.get('https://www.alexamaster.net/sec/image.php') as response:
-#            a = PIL.Image.open(io.BytesIO(await response.content.read()))
-#            a = a.convert('L')  # 处理灰度
-#            a = a.point(lambda _:255 if _ > 150 else 0)
-#            a.save('ocr.png')
+
 #            return pytesseract.image_to_string(a)   
 
 options = selenium.webdriver.ChromeOptions()
@@ -17,8 +11,8 @@ driver = selenium.webdriver.Chrome(options=options)
 driver.get('https://www.alexamaster.net/sec/login.php')
 driver.execute_script('globalThis.scrollTo(0,1000)')
 time.sleep(10)
-driver.find_element_by_css_selector('[placeholder="Your Email ..."]').send_keys('chaowen.guo1@gmail.com')
-driver.find_element_by_css_selector('[placeholder="Your Password ..."]').send_keys('HL798820y+')
+driver.find_element_by_css_selector('input[placeholder="Your Email ..."]').send_keys('chaowen.guo1@gmail.com')
+driver.find_element_by_css_selector('input[placeholder="Your Password ..."]').send_keys('HL798820y+')
 driver.find_element_by_css_selector('input[value="Login Now"]').click()
 time.sleep(60)
 good = PIL.Image.open(io.BytesIO(driver.get_screenshot_as_png()))
