c4464b6ef79e262926778c096c4fb30210f5f55b
chaowen guo <chaowen.guo1@gmail.com>
2021-02-25 10:49:27 -0800

    Update good.py

--- good.py
+++ good.py
@@ -14,7 +14,7 @@ driver.find_element_by_css_selector('input[placeholder="Your Email ..."]').send_
 driver.find_element_by_css_selector('input[placeholder="Your Password ..."]').send_keys(parser.parse_args().password)
 driver.find_element_by_css_selector('input[value="Login Now"]').click()
 time.sleep(60)
-ocr = driver.find_element_by_css_selector('img[src="image.php"]')..
+ocr = driver.find_element_by_css_selector('img[src="image.php"]')
 good = PIL.Image.open(io.BytesIO(driver.get_screenshot_as_png()))
 ocr = good.crop((ocr.location['x'], ocr.location['y'], ocr.location['x'] + ocr.size['width'], ocr.location['y'] + ocr.size['height'])).convert('L').point(lambda _:0 if _ > 160 else 255)
 #driver.find_element_by_css_selector('input[placeholder="Type above number here ..."]').send_keys(pytesseract.image_to_string(ocr))
