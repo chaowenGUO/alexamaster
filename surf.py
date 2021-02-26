87b1e16a55123d3443dc006227401b609f5ab73c
chaowen guo <chaowen.guo1@gmail.com>
2021-02-26 12:24:36 -0800

    Update surf.py

--- surf.py
+++ surf.py
@@ -1,4 +1,4 @@
-import selenium.webdriver, argparse, time
+import selenium.webdriver, argparse, time, pathlib
 parser = argparse.ArgumentParser() 
 parser.add_argument('password')
 options = selenium.webdriver.ChromeOptions()
@@ -22,5 +22,5 @@ time.sleep(10)
 #driver.find_element_by_css_selector('a[onClick^="return visio("]').click()
 #while True: pass
 driver.save_screenshot('ha.png')
-print(driver.page_source)
+pathlib.Path('ha.html').write_text(driver.page_source)
 driver.quit()
