eeb153b2250fd77f8da8dea2725fb09433fb613e
chaowen guo <chaowen.guo1@gmail.com>
2021-02-26 08:14:48 -0800

    Update surf.py

--- surf.py
+++ surf.py
@@ -8,7 +8,8 @@ driver = selenium.webdriver.Chrome(options=options)
 driver.get('https://www.alexamaster.net/Master/157701')
 driver.execute_script('globalThis.open("https://www.websyndic.com")')
 for _ in driver.window_handles:
-    if 'websyndic' in driver.current_url: driver.switch_to.window(_)
+    driver.switch_to.window(_)
+    if 'websyndic' in driver.current_url: break
 driver.save_screenshot('a.png')
 #while True: pass
 driver.quit()
