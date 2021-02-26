8579054a173e7a3fe537a07fa3a93a8587586167
chaowen guo <chaowen.guo1@gmail.com>
2021-02-26 01:50:07 -0800

    Update surf.py

--- surf.py
+++ surf.py
@@ -7,5 +7,7 @@ options.add_argument('--no-first-run')
 driver = selenium.webdriver.Chrome(options=options)
 driver.get('https://www.alexamaster.net/Master/157701')
 driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
+driver.switch_to_window(driver.window_handles[-1])
+driver.save_screen('eb.png')
 while True: pass
 #driver.close()
