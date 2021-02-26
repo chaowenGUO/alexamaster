b2ddc029423a8c773fcebbf41079a32e7df4fc50
chaowen guo <chaowen.guo1@gmail.com>
2021-02-26 01:29:22 -0800

    Update surf.py

--- surf.py
+++ surf.py
@@ -6,5 +6,6 @@ options.add_argument('--incognito')
 options.add_argument('--no-first-run')
 driver = selenium.webdriver.Chrome(options=options)
 driver.get('https://www.alexamaster.net/Master/157701')
+driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
 while True: pass
 #driver.close()
