ea06bb5d06219ddc89608af879b7a4fe8e4414c8
chaowen guo <chaowen.guo1@gmail.com>
2021-02-26 06:46:59 -0800

    Update surf.py

--- surf.py
+++ surf.py
@@ -8,10 +8,10 @@ driver = selenium.webdriver.Chrome(options=options)
 driver.get('https://www.alexamaster.net/Master/157701')
 #driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
 #driver.switch_to_window(driver.window_handles[-1])
-#driver.save_screen('eb.png')
+#driver.save_screenshot('eb.png')
 time.sleep(120)
 for _ in driver.window_handles:
     driver.switch_to_window(_)
-    driver.save_screen(driver.title)
+    driver.save_screenshot(driver.title)
 #while True: pass
 driver.quit()
