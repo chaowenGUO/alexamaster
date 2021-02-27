7ea0d8527b15984093dd8cfe64659fec8197c6db
chaowen guo <chaowen.guo1@gmail.com>
2021-02-27 04:18:26 -0800

    Update new.py

--- new.py
+++ new.py
@@ -1,15 +1,15 @@
-import selenium.webdriver, chrome_webstore_download
+import selenium.webdriver
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--disable-popup-blocking')
 options.add_argument('--incognito')
 options.add_argument('--no-first-run')
+options.add_extension('ebesucher.crx')
 #options.add_argument('--window-size=1960,1080')
 driver = selenium.webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 driver.get('https://www.alexamaster.net/Master/157701')
 driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
-chrome_webstore_download.download('https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk', 'ebesucher')
 #driver.execute_script('globalThis.open("")')
 #for _ in driver.window_handles:
 #    driver.switch_to.window(_)
