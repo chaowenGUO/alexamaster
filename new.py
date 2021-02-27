79c29d50cb9523c82e3b2da8793a36cc30a32947
chaowen guo <chaowen.guo1@gmail.com>
2021-02-27 03:29:49 -0800

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,4 @@
-import selenium.webdriver, argparse, time, pathlib
-parser = argparse.ArgumentParser() 
-parser.add_argument('password')
+import selenium.webdriver
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--disable-popup-blocking')
@@ -11,19 +9,11 @@ driver = selenium.webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 driver.get('https://www.alexamaster.net/Master/157701')
 driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
-driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
-for _ in driver.window_handles:
-    driver.switch_to.window(_)
-    if 'ebesucher' in driver.current_url: break
-#driver.execute_script('globalThis.scrollTo(document.documentElement.scrollWidth,0)')
-#time.sleep(120)
-#driver.find_element_by_id('connect_button').click()
-#driver.find_element_by_id('login_email').send_keys('chaowen.guo1@gmail.com')
-#driver.find_element_by_id('login_passwd').send_keys(parser.parse_args().password)
-#driver.find_element_by_id('connexion').click()
-#driver.find_element_by_id('menu_link_credit').click()
-#driver.find_element_by_css_selector('a[onClick^="return visio("]').click()
-while True: pass
+#driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
+#for _ in driver.window_handles:
+#    driver.switch_to.window(_)
+#    if 'ebesucher' in driver.current_url: break
+#while True: pass
 #driver.save_screenshot('ha.png')
 #pathlib.Path('ha.html').write_text(driver.page_source)
-#driver.quit()
+driver.quit()
