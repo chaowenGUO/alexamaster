531e798ac9cb8a6595f9fc355557d595fd452af9
chaowen guo <chaowen.guo1@gmail.com>
2021-02-27 00:22:46 -0800

    Update surf.py

--- surf.py
+++ surf.py
@@ -13,7 +13,7 @@ driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detai
 for _ in driver.window_handles:
     driver.switch_to.window(_)
     if 'ebesucher' in driver.current_url: break
-driver.execute_script('globalThis.scrollTo(10000,0)')
+driver.execute_script('globalThis.scrollTo(document.body.Width,0)')
 time.sleep(120)
 #driver.find_element_by_id('connect_button').click()
 #driver.find_element_by_id('login_email').send_keys('chaowen.guo1@gmail.com')
