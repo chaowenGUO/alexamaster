2507d9c2815cd5332d423b43d07fa87403e47267
chaowen guo <chaowen.guo1@gmail.com>
2021-02-28 04:53:35 -0800

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-import selenium.webdriver, time, threading, pathlib
+import selenium.webdriver, selenium.webdriver.support.expected_conditions, time, threading, pathlib
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--disable-popup-blocking')
@@ -14,10 +14,10 @@ for _ in driver.window_handles:
     driver.switch_to.window(_)
     if 'ebesucher' in driver.current_url: break
 selenium.webdriver.support.ui.WebDriverWait(driver, 10).until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.XPATH, '//*[text()="Surf now!"]/..'))).click()
-driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[0])
-selenium.webdriver.support.ui.WebDriverWait(driver, 100).until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.ID, "recaptcha-anchor")))
+#driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[0])
+#selenium.webdriver.support.ui.WebDriverWait(driver, 100).until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.ID, "recaptcha-anchor")))
 #pathlib.Path('index.html').write_text(driver.page_source)
 #time.sleep(threading.TIMEOUT_MAX)
-time.sleep(100)
+time.sleep(10)
 driver.save_screenshot('ha.png')
 driver.quit()
