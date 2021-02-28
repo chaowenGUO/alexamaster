965ae50c5511ccf393104f408a7a3666be025eec
chaowen guo <chaowen.guo1@gmail.com>
2021-02-28 04:50:54 -0800

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-import selenium.webdriver, selenium.webdriver.support.expected_conditions, time, threading, pathlib
+import selenium.webdriver, time, threading, pathlib
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--disable-popup-blocking')
@@ -7,7 +7,6 @@ options.add_argument('--no-first-run')
 #options.add_extension('agchmcconfdfcenopioeilpgjngelefk_v1.0.28.4.crx')
 options.add_argument('--window-size=1024,768')
 driver = selenium.webdriver.Chrome(options=options)
-driver.implicitly_wait(10)
 driver.get('https://www.alexamaster.net/Master/157701')
 driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
 driver.execute_script('globalThis.open("http://www.ebesucher.com/surfbar/chaowenguo")')
@@ -16,7 +15,7 @@ for _ in driver.window_handles:
     if 'ebesucher' in driver.current_url: break
 selenium.webdriver.support.ui.WebDriverWait(driver, 10).until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.XPATH, '//*[text()="Surf now!"]/..'))).click()
 driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[0])
-selenium.webdriver.support.ui.WebDriverWait(driver, 10).until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.ID, "recaptcha-anchor")))
+selenium.webdriver.support.ui.WebDriverWait(driver, 100).until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.ID, "recaptcha-anchor")))
 #pathlib.Path('index.html').write_text(driver.page_source)
 #time.sleep(threading.TIMEOUT_MAX)
 time.sleep(100)
