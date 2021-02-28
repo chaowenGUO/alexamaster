f6a93601385b9c764b58e939bb98bd0d6aa840fe
chaowen guo <chaowen.guo1@gmail.com>
2021-02-28 01:51:08 -0800

    Update new.py

--- new.py
+++ new.py
@@ -1,20 +1,19 @@
-import selenium.webdriver
+import selenium.webdriver, time, math
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--disable-popup-blocking')
 options.add_argument('--incognito')
 options.add_argument('--no-first-run')
-options.add_extension('agchmcconfdfcenopioeilpgjngelefk_v1.0.28.4.crx')
+#options.add_extension('agchmcconfdfcenopioeilpgjngelefk_v1.0.28.4.crx')
 #options.add_argument('--window-size=1960,1080')
 driver = selenium.webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 driver.get('https://www.alexamaster.net/Master/157701')
 driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
-#driver.execute_script('globalThis.open("")')
-#for _ in driver.window_handles:
-#    driver.switch_to.window(_)
-#    if 'ebesucher' in driver.current_url: break
-#while True: pass
+driver.execute_script('globalThis.open("http://www.ebesucher.com/surfbar/chaowenguo")')
+for _ in driver.window_handles:
+    driver.switch_to.window(_)
+    if 'ebesucher' in driver.current_url: break
+time.sleep(math.inf)
 #driver.save_screenshot('ha.png')
-#pathlib.Path('ha.html').write_text(driver.page_source)
-driver.quit()
+#driver.quit()
