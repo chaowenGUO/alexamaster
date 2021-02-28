e77912828ae9a400a82ac1c37451cae3398a41ef
chaowen guo <chaowen.guo1@gmail.com>
2021-02-28 02:02:52 -0800

    Update new.py

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ options.add_argument('--disable-popup-blocking')
 options.add_argument('--incognito')
 options.add_argument('--no-first-run')
 #options.add_extension('agchmcconfdfcenopioeilpgjngelefk_v1.0.28.4.crx')
-#options.add_argument('--window-size=1960,1080')
+options.add_argument('--window-size=1024,768')
 driver = selenium.webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 driver.get('https://www.alexamaster.net/Master/157701')
@@ -14,6 +14,6 @@ driver.execute_script('globalThis.open("http://www.ebesucher.com/surfbar/chaowen
 for _ in driver.window_handles:
     driver.switch_to.window(_)
     if 'ebesucher' in driver.current_url: break
-time.sleep(threading.TIMEOUT_MAX)
-#driver.save_screenshot('ha.png')
-#driver.quit()
+#time.sleep(threading.TIMEOUT_MAX)
+driver.save_screenshot('ha.png')
+driver.quit()
