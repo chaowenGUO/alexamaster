cadd1b7be5190b807e3440a650526e0e587ae1f2
chaowen guo <chaowen.guo1@gmail.com>
2021-03-01 08:15:59 -0800

    Update surf.py

--- surf.py
+++ surf.py
@@ -9,15 +9,12 @@ options.add_argument('--no-first-run')
 options.add_argument('--disable-dev-shm-usage')
 #options.add_argument('--window-size=1960,1080')
 driver = selenium.webdriver.Chrome(options=options)
-driver.implicitly_wait(10)
 driver.get('https://www.alexamaster.net/Master/157701')
-driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
-driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
-for _ in driver.window_handles:
-    driver.switch_to.window(_)
-    if 'ebesucher' in driver.current_url: break
-#driver.execute_script('globalThis.scrollTo(document.documentElement.scrollWidth,0)')
-#time.sleep(120)
+#driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
+#driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
+#for _ in driver.window_handles:
+#    driver.switch_to.window(_)
+#    if 'ebesucher' in driver.current_url: break
 #driver.find_element_by_id('connect_button').click()
 #driver.find_element_by_id('login_email').send_keys('chaowen.guo1@gmail.com')
 #driver.find_element_by_id('login_passwd').send_keys(parser.parse_args().password)
