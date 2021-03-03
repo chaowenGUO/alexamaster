1a1c5a38530ebf4431314d6e2452f8325a996604
chaowen guo <chaowen.guo1@gmail.com>
2021-03-03 12:48:43 -0600

    Update surf.py

--- surf.py
+++ surf.py
@@ -1,6 +1,4 @@
-import selenium.webdriver, argparse, time, threading
-parser = argparse.ArgumentParser() 
-parser.add_argument('password')
+import selenium.webdriver, time
 options = selenium.webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--disable-popup-blocking')
@@ -10,18 +8,19 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
 #options.add_argument('--window-size=1960,1080')
 driver = selenium.webdriver.Chrome(options=options)
-driver.get('https://www.alexamaster.net/Master/157701')
-driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
 #driver.execute_script('globalThis.open("https://chrome.google.com/webstore/detail/ebesucher-addon/agchmcconfdfcenopioeilpgjngelefk")')
 #for _ in driver.window_handles:
 #    driver.switch_to.window(_)
 #    if 'ebesucher' in driver.current_url: break
 #driver.find_element_by_id('connect_button').click()
-#driver.find_element_by_id('login_email').send_keys('chaowen.guo1@gmail.com')
-#driver.find_element_by_id('login_passwd').send_keys(parser.parse_args().password)
+#driver.find_element_by_id('login_email').send_keys('c#driver.find_element_by_id('login_passwd').send_keys(parser.parse_args().password)
 #driver.find_element_by_id('connexion').click()
 #driver.find_element_by_id('menu_link_credit').click()
-#driver.find_element_by_css_selector('a[onClick^="return visio("]').click()
-time.sleep(threading.TIMEOUT_MAX)
+#driver.find_element_by_css_selector('a[onClick^="return visio("]').click()i
+while True:
+    driver.get('https://www.alexamaster.net/Master/157701')
+    driver.execute_script('globalThis.open("http://www.crunchingbaseteam.com/view.php?user=chaowenguo")')
+    while driver.title == 'Surfing...': time.sleep(60)
+    break
 #driver.save_screenshot('ha.png')
 #driver.quit()
