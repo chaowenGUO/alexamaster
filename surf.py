3723a33bfbe9afdbf46346d6ba0016c1269d8d09
chaowen guo <chaowen.guo1@gmail.com>
2021-03-04 14:17:18 -0600

    Update surf.py

--- surf.py
+++ surf.py
@@ -23,7 +23,6 @@ while True:
         driver.switch_to.window(_)
         driver.close()
     driver.switch_to.window(driver.window_handles[0])
-    time.sleep(20)
-    if driver.title == 'Surfing Stopped!': driver.switch_to.active_element.click()
+    driver.refresh()
 #driver.save_screenshot('ha.png')
 #driver.quit()
