133f012d852bb40b979686ef6bbbb5b97be0807a
chaowen guo <chaowen.guo1@gmail.com>
2021-03-05 17:50:53 -0600

    Update surf.py

--- surf.py
+++ surf.py
@@ -9,7 +9,7 @@ async def f():
         crunchingbaseteam = await browser.new_page()
         await crunchingbaseteam.goto('http://www.crunchingbaseteam.com/view.php?user=chaowenguo', timeout=0)
         while True:
-            await asyncio.sleep(300)
+            await asyncio.sleep(600)
             for page in itertools.islice(context.pages, 1, len(context.pages) - 1): await page.close()
 
 asyncio.run(f())
