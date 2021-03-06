dec10554857b57db39e092d37bfa385a227f77f8
chaowen guo <chaowen.guo1@gmail.com>
2021-03-05 19:53:39 -0600

    Update surf.java

--- surf.java
+++ surf.java
@@ -1,1 +1,13 @@
-
+public class Main
+{
+    public static void main(java.lang.String[] args)
+    {
+        try (final var playwright = com.microsoft.playwright.Playwright.create())
+        {
+            final var browser = playwright.chromium().launch();
+            final var page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            java.lang.System.out.println(page.title());
+        }
+    }
+}
