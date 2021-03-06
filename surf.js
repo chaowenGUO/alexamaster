2ce3683a82ae4f8693f58c49851321d17c1c0724
chaowen guo <chaowen.guo1@gmail.com>
2021-03-05 22:11:57 -0600

    Update surf.js

--- surf.js
+++ surf.js
@@ -1,6 +1,6 @@
 import playwright from 'playwright'
 
-const browser = await playwright.chromium.launch({executablePath:'/opt/google/chrome/chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
+const browser = await playwright.chromium.launch({executablePath:'/usr/bin/google-chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const context = await browser.newContext()
 const alexamaster = await context.newPage()
 await alexamaster.goto('https://www.alexamaster.net/Master/157701')
