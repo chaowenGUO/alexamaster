52e13d6691c632d63b5187d5ae6dd0a8e5b01ffb
chaowen guo <chaowen.guo1@gmail.com>
2021-03-08 16:32:51 -0600

    Update surf.js

--- surf.js
+++ surf.js
@@ -1,4 +1,4 @@
-import chromium from 'playwright-chromium'
+import {chromium} from 'playwright-chromium'
 
 const browser = await chromium.launch({executablePath:'/usr/bin/google-chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const context = await browser.newContext()
