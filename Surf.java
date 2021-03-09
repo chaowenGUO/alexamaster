ca0e0b6b2b434345274deee3670aab4532707bdd
chaowen guo <chaowen.guo1@gmail.com>
2021-03-09 13:19:52 -0600

    Update Surf.java

--- Surf.java
+++ Surf.java
@@ -4,7 +4,7 @@ public class Surf
     {
         try (final var playwright = com.microsoft.playwright.Playwright.create())
         {
-            final var browser = playwright.chromium().launch(new com.microsoft.playwright.BrowserType.LaunchOptions().setExecutablePath(java.nio.file.Path.of('/usr/bin/google-chrome')).setArgs(java.util.List.of('--disable-gpu')));
+            final var browser = playwright.chromium().launch(new com.microsoft.playwright.BrowserType.LaunchOptions().setExecutablePath(java.nio.file.Path.of("/usr/bin/google-chrome")).setArgs(java.util.List.of("--disable-gpu")));
             final var page = browser.newPage();
             page.navigate("http://playwright.dev");
             java.lang.System.out.println(page.title());
