d744496e5cdbc19ba1e04e4dbbaa68e9b52357d6
chaowen guo <chaowen.guo1@gmail.com>
2021-03-08 18:07:04 -0600

    Update colab.js

--- colab.js
+++ colab.js
@@ -1,6 +1,6 @@
 import {chromium} from 'playwright-chromium'
 
-const browser = await chromium.launch({args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
+const browser = await chromium.launch({executablePath:'/usr/lib/chromium-browser/chromium-browser', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const context = await browser.newContext()
 const alexamaster = await context.newPage()
 await alexamaster.goto('https://www.alexamaster.net/Master/157701')
