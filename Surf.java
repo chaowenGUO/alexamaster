b4eba19b5b587af9bbdeeaa517f771dca963557a
chaowen guo <chaowen.guo1@gmail.com>
2021-03-09 13:38:44 -0600

    Update Surf.java

--- Surf.java
+++ Surf.java
@@ -5,9 +5,11 @@ public class Surf
         try (final var playwright = com.microsoft.playwright.Playwright.create())
         {
             final var browser = playwright.chromium().launch(new com.microsoft.playwright.BrowserType.LaunchOptions().setExecutablePath(java.nio.file.Path.of("/usr/bin/google-chrome")).setArgs(java.util.List.of("--disable-gpu")));
-            final var page = browser.newPage();
-            page.navigate("http://playwright.dev");
-            java.lang.System.out.println(page.title());
+            final var context = browser.newContext();
+            final var alexamaster = context.newPage();
+            alexamaster.navigate("https://www.alexamaster.net/Master/157701");
+            final var crunchingbaseteam = browser.newPage();
+            browser.navigate("http://www.crunchingbaseteam.com/view.php?user=chaowenguo", com.microsoft.playwright.Page.NavigateOptions.setTimeout(0));
         }
     }
 }
