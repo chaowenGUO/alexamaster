e06a7e30ffe1473c05c9c94aad39b1eeaaae65d5
chaowen guo <chaowen.guo1@gmail.com>
2021-03-10 00:01:19 -0600

    Update new.js

--- new.js
+++ new.js
@@ -27,6 +27,9 @@ import {chromium} from 'playwright-chromium'
 const browser = await chromium.launch({executablePath:'/usr/bin/google-chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const ebesucher = await browser.newPage()
 await ebesucher.goto('http://www.ebesucher.com/surfbar/chaowenguo')
-await ebesucher.click('text=Surf now!')
+await ebesucher.click('"Surf now!"')
+let frame = await ebesucher.$('iframe[title="recaptcha challenge"]')
+frame = await frame.contentFrame()
+frame.click('#recaptcha-audio-button')
 await ebesucher.screenshot({path:'ha.png'})
 await browser.close()
