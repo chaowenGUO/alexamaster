fb6c89bbbf79ff19a4cd1618aaf908420a29acc4
chaowen guo <chaowen.guo1@gmail.com>
2021-03-10 17:58:56 -0600

    Update Version.java

--- Version.java
+++ Version.java
@@ -3,6 +3,6 @@ public class Version
     public static void main(java.lang.String[] args) throws java.lang.Exception
     {
         final var processBuilder = new java.lang.ProcessBuilder("docker", "run", "--rm", "openjdk:slim", "java", "--version");
-        java.nio.file.Files.writeString(java.nio.file.Path.of(java.lang.System.getenv('GITHUB_ENV')), 'JAVA =' + new java.lang.String(processBuilder.start().getInputStream().readAllBytes(), java.nio.charset.StandardCharsets.UTF_8).split(" ")[1]);
+        java.nio.file.Files.writeString(java.nio.file.Path.of(java.lang.System.getenv("GITHUB_ENV")), "JAVA=" + new java.lang.String(processBuilder.start().getInputStream().readAllBytes(), java.nio.charset.StandardCharsets.UTF_8).split(" ")[1]);
     }
 }       
