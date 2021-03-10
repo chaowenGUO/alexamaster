34043096ace7470c71db43bbccd210e2dfd4a229
chaowen guo <chaowen.guo1@gmail.com>
2021-03-09 23:12:42 -0600

    Update new.js

--- new.js
+++ new.js
@@ -27,6 +27,6 @@ import {chromium} from 'playwright-chromium'
 const browser = await chromium.launch({executablePath:'/usr/bin/google-chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const ebesucher = await browser.newPage()
 await ebesucher.goto('http://www.ebesucher.com/surfbar/chaowenguo')
-await page.click('text=Log in');
+await page.click('text=Surf now!');
 await ebesucher.screenshot({path:'ha.png'})
 await browser.close()
