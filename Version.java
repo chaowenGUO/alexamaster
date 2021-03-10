fd7ea25d972b600933e21b23f2462ac6b0ac037b
chaowen guo <chaowen.guo1@gmail.com>
2021-03-10 17:21:58 -0600

    Update Version.java

--- Version.java
+++ Version.java
@@ -3,6 +3,6 @@ public class Version
     public static void main(java.lang.String[] args) throws java.lang.Exception
     {
         final var processBuilder = new java.lang.ProcessBuilder("docker", "run", "--rm", "openjdk:slim", "java", "--version");
-        java.lang.System.out.println(new java.lang.String(processBuilder.start().getInputStream().readAllBytes(), java.nio.charset.StandardCharsets.UTF_8));
+        java.lang.System.out.println(new java.lang.String(processBuilder.start().getInputStream().readAllBytes(), java.nio.charset.StandardCharsets.UTF_8).split(" ")[1]);
     }
 }       
