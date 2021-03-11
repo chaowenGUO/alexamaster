4077a70e71245cfdd869788ba02f67fe4ea32900
chaowen guo <chaowen.guo1@gmail.com>
2021-03-11 15:25:51 -0600

    Update version.py

--- version.py
+++ version.py
@@ -3,6 +3,6 @@ import asyncio, pathlib, os
 async def f():
     process = await asyncio.create_subprocess_exec('docker', 'run', '--rm', 'python:slim', 'python', '-c', 'import platform; print(platform.python_version())', stdout=asyncio.subprocess.PIPE)
     stdout, _ = await process.communicate()
-    print(stdout.decode(), flush=True)
+    pathlib.Path(os.getenv('GITHUB_ENV')).write_text('PY=' + stdout.decode())
     
 asyncio.run(f())
