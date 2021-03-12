aa1b1df91bcbe9e3b5559c8ad5038a0aab1d23f3
chaowen guo <chaowen.guo1@gmail.com>
2021-03-11 21:20:54 -0600

    Update new.js

--- new.js
+++ new.js
@@ -1,10 +1,8 @@
 import {chromium} from 'playwright-chromium'
 
 const browser = await chromium.launch({executablePath:'/usr/bin/google-chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
-const context = await browser.newContext()
+const context = await browser.newContext({storageState:{cookies:[{name:'device', value:'jc7VYKQxoMWrzY7095d3Ir', domain:'www.ebesucher.com', path:'/'}]}})
 const ebesucher = await browser.newPage()
-await ebesucher.goto('http://www.ebesucher.com/surfbar/chaowenguo')
-await context.addCookies([{name:'device', value:'jc7VYKQxoMWrzY7095d3Ir', domain:'www.ebesucher.com', path:'/'}])
 await ebesucher.goto('http://www.ebesucher.com/surfbar/chaowenguo')
 /*await ebesucher.click('span:text-is("Surf now!")') 
 let frame = await ebesucher.waitForSelector('iframe[title="recaptcha challenge"]')
