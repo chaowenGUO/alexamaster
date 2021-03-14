be85b4fbb553241a5e88fbd86d1e92abff391ea3
chaowen guo <chaowen.guo1@gmail.com>
2021-03-13 19:02:13 -0600

    Update heroku.js

--- js/heroku.js
+++ js/heroku.js
@@ -1,4 +1,7 @@
 import http from 'http'
 import process from 'process'
-import './surf.js'
-http.createServer().listen(process.env.PORT)
+http.createServer((req, res) =>
+{
+    res.write('Hello World!')
+    res.end()
+}).listen(process.env.PORT)
