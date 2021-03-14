74dc7c4ec30403a53e7e1d55a247f022a12e08f2
chaowen guo <chaowen.guo1@gmail.com>
2021-03-13 19:52:55 -0600

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -1,4 +1,4 @@
 import http from 'http'
 import process from 'process'
 import './surf.js'
-http.createServer().listen(process.env.PORT)
+http.createServer((req, res) => res.end()).listen(process.env.PORT)
