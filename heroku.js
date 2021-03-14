f1120ab74b710a8abc3f355a04c4ee83c651e524
chaowen guo <chaowen.guo1@gmail.com>
2021-03-14 00:56:29 -0600

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -1,3 +1,3 @@
 import http from 'http'
 import process from 'process'
-http.createServer((req, res) => res.end()).listen(process.env.PORT, () => globalThis.console.log('jaja'))
+http.createServer().listen(process.env.PORT, () => globalThis.console.log('jaja'))
