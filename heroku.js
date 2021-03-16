0fae21dc293b1299943b5af11f182933545620ca
chaowen guo <chaowen.guo1@gmail.com>
2021-03-15 22:42:25 -0500

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -3,5 +3,6 @@ import {chromium} from 'playwright-chromium'
 const browser = await chromium.launch({executablePath:'/usr/bin/google-chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const staroid = await browser.newPage()
 await staroid.goto('https://staroid.com/g/chaowenGUOorg/alexamaster/release')
+await staroid.click('span:text_is("Sign in with Github")')
 await staroid.screenshot({path:'ha.png'})
 await browser.close()
