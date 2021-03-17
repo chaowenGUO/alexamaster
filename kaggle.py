30ae90eb9fd591a2801cb0df869d9c25908e4ffc
chaowen guo <chaowen.guo1@gmail.com>
2021-03-16 19:59:41 -0500

    Update and rename kaggle.ipynb to kaggle.py

---
+++ kaggle.py
@@ -0,0 +1,14 @@
+import aiohttp, pathlib, asyncio， sys
+
+async def f():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb') as resp:
+            pathlib.Path('google-chrome-stable_current_amd64.deb').write_bytes(await resp.content.read())
+    await asyncio.create_subprocess_exec('apt', 'install', '-y', '--no-install-recommends', './google-chrome-stable_current_amd64.deb')
+    await asyncio.create_subprocess_exec('pip', 'install', 'playwright')
+    await asyncio.create_subprocess_exec('git', 'clone', 'git://github.com/chaowenguoorg/alexamaster')
+    await asyncio.create_subprocess_exec('python', 'alexamaster/py/surf.py')
+    asyncio.sleep(8 * 60 * 60)
+    sys.exit()
+
+asyncio.run(f())
