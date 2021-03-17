8accd46fee79322955e02f25939dbc9a64329465
chaowen guo <chaowen.guo1@gmail.com>
2021-03-16 21:00:46 -0500

    Update kaggle.py

--- kaggle.py
+++ kaggle.py
@@ -4,12 +4,13 @@ async def f():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb') as resp:
             pathlib.Path('google-chrome-stable_current_amd64.deb').write_bytes(await resp.content.read())
-    await asyncio.create_subprocess_exec('apt', 'install', '-y', '--no-install-recommends', './google-chrome-stable_current_amd64.deb')
+    chrome = await asyncio.create_subprocess_exec('apt', 'install', '-y', '--no-install-recommends', './google-chrome-stable_current_amd64.deb')
     await asyncio.create_subprocess_exec('pip', 'install', 'playwright')
     await asyncio.create_subprocess_exec('git', 'clone', 'git://github.com/chaowenguoorg/alexamaster')
     #await asyncio.create_subprocess_exec('python', 'alexamaster/py/surf.py')
     #await asyncio.sleep(8 * 60 * 60)
     #sys.exit()
+    chrome.wait()
     process =  await asyncio.create_subprocess_exec('google-chrome', '--version', stdout=asyncio.subprocess.PIPE)
     stdout, _ = await process.communicate()
     print(stdout, flush=True),
