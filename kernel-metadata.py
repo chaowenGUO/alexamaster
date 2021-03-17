66e246820e0297602593e80ab3f20a9356622787
chaowen guo <chaowen.guo1@gmail.com>
2021-03-16 22:21:39 -0500

    Update kernel-metadata.py

--- kernel-metadata.py
+++ kernel-metadata.py
@@ -1,3 +1,7 @@
+import fileinput, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('version')
 with fileinput.FileInput('kernel-metadata.json', inplace=True) as file:
     for line in file:
-        print(line.replace('VERSION', 'chat.js'), end='')
+        print(line.replace('VERSION', parser.parse_args().version), end='')
