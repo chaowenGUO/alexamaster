2c1e3364e9e734e60d01627ba18b2e11d9f082cf
chaowen guo <chaowen.guo1@gmail.com>
2021-03-16 20:36:19 -0500

    Update kaggle.py

--- kaggle.py
+++ kaggle.py
@@ -8,7 +8,7 @@ async def f():
     await asyncio.create_subprocess_exec('pip', 'install', 'playwright')
     await asyncio.create_subprocess_exec('git', 'clone', 'git://github.com/chaowenguoorg/alexamaster')
     await asyncio.create_subprocess_exec('python', 'alexamaster/py/surf.py')
-    await asyncio.sleep(8 * 60 * 60)
-    sys.exit()
+    #await asyncio.sleep(8 * 60 * 60)
+    #sys.exit()
 
 asyncio.run(f())
