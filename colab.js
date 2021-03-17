d315b8bc19608cf4172522f573f771be532eea61
chaowen guo <chaowen.guo1@gmail.com>
2021-03-17 00:27:56 -0500

    Update colab.js

--- colab.js
+++ colab.js
@@ -1,7 +1,11 @@
 import {chromium} from 'playwright-chromium'
 
 const browser = await chromium.launch({executablePath:'/usr/bin/google-chrome', args:['--disable-gpu']})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
-const page = await browser.newPage()
-await page.goto('https://colab.research.google.com/github/chaowenGUOorg/alexamaster/blob/main/colab.ipynb')
-await page.keyboard.press('Control+F9')
+const context = await browser.newContext()
+for (const _ of Array(5).keys())
+{
+    const page = await context.newPage()
+    await page.goto('https://colab.research.google.com/github/chaowenGUOorg/alexamaster/blob/main/colab.ipynb')
+    await page.keyboard.press('Control+F9')
+}
 await browser.close()
