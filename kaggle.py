f1c7c570c3825d0ab8e862444959843e4b27a631
chaowen guo <chaowen.guo1@gmail.com>
2021-03-16 20:44:31 -0500

    Update kaggle.py

--- kaggle.py
+++ kaggle.py
@@ -4,7 +4,7 @@ async def f():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb') as resp:
             pathlib.Path('google-chrome-stable_current_amd64.deb').write_bytes(await resp.content.read())
-    await asyncio.create_subprocess_exec('apt', 'install', '-y', '--no-install-recommends', './google-chrome-stable_current_amd64.deb')
+    process = await asyncio.create_subprocess_exec('apt', 'install', '-y', '--no-install-recommends', './google-chrome-stable_current_amd64.deb', stdout=asyncio.subprocess.PIPE)
     await asyncio.create_subprocess_exec('pip', 'install', 'playwright')
     await asyncio.create_subprocess_exec('git', 'clone', 'git://github.com/chaowenguoorg/alexamaster')
     #await asyncio.create_subprocess_exec('python', 'alexamaster/py/surf.py')
